347562c2e94a7d312bf826ecfb0a291bd3268c33
Adriaan van Rossum <git@adriaan.io>
2020-05-07 13:24:52 +0200

    Add multiple pages

---
+++ pages/invest/company.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import Layout from "../../components/layout";
+
+export default function InvestCompany() {
+  return (
+    <Layout>
+      <Head>
+        <title>Invest company</title>
+      </Head>
+      <h1>Invest company</h1>
+      <p>
+        A full-service exchange platform for tokenized securities and digital
+        assets
+      </p>
+    </Layout>
+  );
+}
