c49cfbd7305aad3663a41069305b06910c41a741
Adriaan van Rossum <git@adriaan.io>
2020-05-07 16:45:30 +0200

    Add API

--- pages/invest/company.js
+++ pages/invest/company.js
@@ -1,13 +1,17 @@
 import Head from "next/head";
 import Layout from "../../components/layout";
+import useSWR from "swr";
 
 export default function InvestCompany() {
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const { data, error } = useSWR("/api/company", fetcher);
+
   return (
     <Layout>
       <Head>
         <title>Invest company</title>
       </Head>
-      <h1>Invest company</h1>
+      <h1>{data ? `Invest in ${data.name}` : "Loading..."}</h1>
       <p>
         A full-service exchange platform for tokenized securities and digital
         assets
