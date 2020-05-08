b0cffb602b59ad3d991c9254ffb068316c2ff887
Adriaan van Rossum <git@adriaan.io>
2020-05-08 14:46:33 +0200

    Cleanup

--- pages/_app.js
+++ pages/_app.js
@@ -11,20 +11,3 @@ class MyApp extends App {
 }
 
 export default appWithTranslation(MyApp);
-
-// import React from "react";
-// import App from "next/app";
-// import { appWithTranslation } from "../initializers/i18n";
-// import "../styles/global.css";
-
-// function MyApp({ Component, pageProps }) {
-//   return <Component {...pageProps} />;
-// }
-
-// MyApp.getInitialProps = async (appContext) => {
-//   const appProps = await App.getInitialProps(appContext);
-//   console.log("appProps", appProps);
-//   return { ...appProps };
-// };
-
-// export default appWithTranslation(MyApp);
